a85a101a6f661a2e543f0503efbbc2f4fb43d7e1
Andy Wright <atwright147@gmail.com>
2017-03-03 08:50:16 +0000

    updates

--- node.dockerfile
+++ node.dockerfile
@@ -3,9 +3,8 @@ FROM node:6.9.1
 
 ADD ./package.json /app/package.json
 
-RUN apt-get update \
-    && npm install -g forever --silent --progress=false \
-    && npm install --silent --progress=false
+RUN npm install -g forever --silent --progress=false && \
+    npm install --silent --progress=false
 
 WORKDIR /app
 
--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.get('/data', function (req, res) {
+    res.send({
+        "key1": "value1",
+        "key2": "value2",
+        "key3": "value3"
+    });
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
