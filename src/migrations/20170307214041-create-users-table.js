cb3fcf3f53e060a06f2ee9b995a790db36a63ee1
Andy Wright <atwright147@gmail.com>
2017-03-12 00:08:53 +0000

    make user fields not nullable

--- src/migrations/20170307214041-create-users-table.js
+++ src/migrations/20170307214041-create-users-table.js
@@ -2,11 +2,11 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('users', function(table) {
             table.increments('id').primary();
-            table.string('firstName');
-            table.string('lastName');
-            table.string('email');
-            table.string('password');
-            table.integer('active');
+            table.string('firstName').notNullable();
+            table.string('lastName').notNullable();
+            table.string('email').notNullable();
+            table.string('password').notNullable();
+            table.integer('active').notNullable().defaultTo(0);
         });
 };
 exports.down = function(knex) {
