8d9b9d4b90bf1e8c985ea37136dcd7e51ac94b1c
Andy Wright <atwright147@gmail.com>
2017-03-29 01:06:22 +0100

    add `fetch()` to querys (oops)

--- src/controllers/groups.js
+++ src/controllers/groups.js
@@ -9,6 +9,7 @@ const groupsController = {
     list: (req, res) => (
         Group
             .where('id', req.params.id)
+            .fetch()
             .then(result => res.status(200).json(result))
             .catch(error => res.status(400).send(error))
     ),
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -16,6 +16,7 @@ const usersController = {
     list: (req, res) => (
         User
             .where('id', Number(req.params.id))
+            .fetch()
             .then(result => res.status(200).json(result))
             .catch(error => res.status(400).send(error))
     ),
