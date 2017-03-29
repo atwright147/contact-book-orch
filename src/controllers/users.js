28036bff1f55a780e6dcff9f04873a8ece9d6c7e
Andy Wright <atwright147@gmail.com>
2017-03-29 01:05:58 +0100

    convert id param to number

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -15,7 +15,7 @@ const usersController = {
      */
     list: (req, res) => (
         User
-            .where('id', req.params.id)
+            .where('id', Number(req.params.id))
             .then(result => res.status(200).json(result))
             .catch(error => res.status(400).send(error))
     ),
